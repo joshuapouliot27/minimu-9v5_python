7abefd59d7b0d41bcf62ea8da460b921358dd631
Joshua Pouliot <joshuapouliot27@gmail.com>
2018-04-01 22:23:00 -0400

    Updated code.

--- calibrate.py
+++ calibrate.py
@@ -41,11 +41,12 @@ class imu_data_obj:
 log_formatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
 root_logger = logging.getLogger()
 
-file_handler = logging.FileHandler("{0}/{1}.log".format("./", "log"))
+file_handler = logging.FileHandler("log.log")
 file_handler.setFormatter(log_formatter)
 root_logger.addHandler(file_handler)
 
-console_handler = logging.StreamHandler(sys.stdout)
+stdscr = curses.initscr()
+console_handler = logging.StreamHandler(stdscr)
 console_handler.setFormatter(log_formatter)
 root_logger.addHandler(console_handler)
 
