c1d4cf21dd83c0ace8c589d9e0955c6796adb12a
Joshua Pouliot <joshuapouliot27@gmail.com>
2018-04-01 22:17:05 -0400

    Updated code.

--- calibrate.py
+++ calibrate.py
@@ -4,6 +4,8 @@ import platform
 
 import time
 
+import sys
+
 from LIS3MDL import LIS3MDL
 from LSM6DS33 import LSM6DS33
 
@@ -39,11 +41,11 @@ class imu_data_obj:
 log_formatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
 root_logger = logging.getLogger()
 
-file_handler = logging.FileHandler("{0}/{1}.log".format("./", "log.log"))
+file_handler = logging.FileHandler("{0}/{1}.log".format("./", "log"))
 file_handler.setFormatter(log_formatter)
 root_logger.addHandler(file_handler)
 
-console_handler = logging.StreamHandler()
+console_handler = logging.StreamHandler(sys.stdout)
 console_handler.setFormatter(log_formatter)
 root_logger.addHandler(console_handler)
 
